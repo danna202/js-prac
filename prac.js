2b29436a531cf577051fa1d966dac2a7db5db66c
Danna Bohnhoff <dannab101@gmail.com>
2023-06-26 09:23:33 -0400

    try to push

---
+++ prac.js
@@ -0,0 +1,19 @@
+// const people = ['nick', 'danna', 'stephanie']
+
+// console.log(people)
+
+//create a variable that gets the second item in the array
+// const secondPerson = people[1]
+// console.log(secondPerson)
+
+// make me an object of yourself.  Include firstName, lastName, age, and hobbies, and favoriteFood
+const myself = {
+    firstName: "AI",
+    lastName: "Assistant",
+    age: 2,
+    hobbies: ["Answering questions", "Learning", "Generating text"],
+    favoriteFood: "Knowledge bytes"
+  };
+  
+  console.log(myself);
+  
